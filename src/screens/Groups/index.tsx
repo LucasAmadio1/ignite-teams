5f287a8d58da562ce465ae274ac5ffaea339ada5
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-11-03 10:43:27 -0300

    componentes de lista

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
+import { GroupCard } from '../../components/GroupCard'
 import { Header } from '../../components/Header'
 import { Highlight } from '../../components/Highlight'
 import { Container } from './styles'
+import { FlatList } from 'react-native'
+import { ListEmpty } from '../../components/ListEmpty'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   )
 }
